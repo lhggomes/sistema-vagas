d11e536d58a72114b900aea085146cb4f8e154fc
Lucas Henrique Silva Gomes <lucas.gomes-partner@aperam.com>
2020-12-06 09:08:32 -0300

    Adding class models

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+from django.db.models import Model, CharField, FloatField, TextField, ForeignKey
 
-# Create your models here.
+
+class Vaga(Model):
+    """
+    Vaga Model Class
+    """
+    descricao = CharField(max_length=100, verbose_name="descrição")
+    salario = FloatField()
+    requisitos = TextField()
+    escolaridade = CharField()
+
+    def __str__(self):
+        return "{} ({})".format(self.descricao, self.salario)
+
+
+class Empresa(Model):
+    razao_social = CharField(max_length=255, verbose_name="razão social")
+    cnpj = CharField(max_length=14)
+
+    def __str__(self):
+        return "{} - {}".format(self.razao_social, self.cnpj)
+
+
+class Candidato(Model):
+    nome = CharField(max_length=100)
+    email = CharField(max_length=200)
+
+
+class Candidatura(Model):
+    candidato = CharField(max_length=100)
+    vaga = CharField(max_length=50)
+    experiencia = CharField(max_length=255, verbose_name="experiência")
+    ultima_escolaridade = CharField(max_length=60)
+    pretensao_salarial = FloatField(verbose_name="pretensão salarial")
+
+    def __str__(self):
+        return "{} - {}  - {}".format(self.candidato, self.pretensao_salarial, self.ultima_escolaridade)
