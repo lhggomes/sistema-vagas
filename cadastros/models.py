439da8de39dad8956ea05a6cea985240f99b984d
Lucas Henrique Silva Gomes <lucas.gomes-partner@aperam.com>
2020-12-06 09:53:13 -0300

    Model Class

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,17 +1,4 @@
-from django.db.models import Model, CharField, FloatField, TextField, ForeignKey
-
-
-class Vaga(Model):
-    """
-    Vaga Model Class
-    """
-    descricao = CharField(max_length=100, verbose_name="descrição")
-    salario = FloatField()
-    requisitos = TextField()
-    escolaridade = CharField()
-
-    def __str__(self):
-        return "{} ({})".format(self.descricao, self.salario)
+from django.db.models import Model, CharField, FloatField, TextField, ForeignKey, EmailField, PROTECT
 
 
 class Empresa(Model):
@@ -21,15 +8,28 @@ class Empresa(Model):
     def __str__(self):
         return "{} - {}".format(self.razao_social, self.cnpj)
 
+class Vaga(Model):
+    """
+    Vaga Model Class
+    """
+    descricao = CharField(max_length=100, verbose_name="descrição")
+    salario = FloatField()
+    requisitos = TextField()
+    escolaridade = CharField(max_length=100)
+    empresa = ForeignKey(Empresa, on_delete=PROTECT)
+
+    def __str__(self):
+        return "{} ({})".format(self.descricao, self.salario)
+
 
 class Candidato(Model):
     nome = CharField(max_length=100)
-    email = CharField(max_length=200)
+    email = EmailField()
 
 
 class Candidatura(Model):
-    candidato = CharField(max_length=100)
-    vaga = CharField(max_length=50)
+    candidato = ForeignKey(Candidato, on_delete=PROTECT)
+    vaga = ForeignKey(Vaga, on_delete=PROTECT)
     experiencia = CharField(max_length=255, verbose_name="experiência")
     ultima_escolaridade = CharField(max_length=60)
     pretensao_salarial = FloatField(verbose_name="pretensão salarial")
