a92c2ec89fd5abe64a75ba606fe0d065008f5c56
Lucas Henrique Silva Gomes <lucas.gomes-partner@aperam.com>
2020-12-06 21:50:59 -0300

    Doing the process to make the links works

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CandidatoCreate, VagaCreate, CandidaturaCrate, VagaUpdate
+from .views import CandidatoCreate, VagaCreate, CandidaturaCrate, VagaUpdate, EmpresaCreate
 
 urlpatterns = [
     path('cadastrar/candidato/', CandidatoCreate.as_view(), name="cad-candidato"),
     path('cadastrar/vaga/', VagaCreate.as_view(), name="cad-vaga"),
+    path('cadastrar/empresa/', EmpresaCreate.as_view(), name="cad-empresa"),
     path('cadastrar/candidatura', CandidaturaCrate.as_view(), name="cad-candidatura"),
 
     path('editar/campo/<int:pk>', VagaUpdate.as_view(), name="editar-campo"),
